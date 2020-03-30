c77d3ebb025beb1d5de33ce41fd0c07449ca8eda
Fernando <50506780+FernandoPZ@users.noreply.github.com>
2020-03-29 19:47:39 -0600

    Avance actualizado

--- application/controllers/alumnos.py
+++ application/controllers/alumnos.py
@@ -1,57 +1,145 @@
-import web # pip install web.py
-import app02 # app.py
-import csv # CSV parser
-import json # json parser
+import web
+import app02
+import json
+import csv
 
-'''
-Controller Alumnos que es invocado cuando el usuario
-ingrese a la URL: http://localhost:8080/alumnos?action=get&token=1234
-'''
 class Alumnos:
-
-    def __init__(self): # Método inicial o constructor de la clase
-        pass # Simplemente continua con la ejecución
-
     def GET(self):
         try:
-            data = web.input() # recibe los datos por la url
-            if data['token'] == "1234": # valida el token que se recibe por url
-                if data['action'] == 'get': # evalua la acción a realizar
-                    result = self.actionGet() # llama al metodo actionGet(), y almacena el resultado
-                    return json.dumps(result) # Parsea el diccionario result a formato json
-                else:
-                    result = {} # crear diccionario vacio
-                    result['status'] = "Command not found"
-                    return json.dumps(result) # Parsea el diccionario result a formato json
-            else:
-                result = {} # crear diccionario vacio
-                result['status'] = "Invalid Token"
-                return json.dumps(result) # Parsea el diccionario result a formato json
-        except Exception as e:
-            result = {} # crear diccionario vacio
-            result['status'] = "Values missing, sintaxis: alumnos?action=get&token=XXXX"
-            return json.dumps(result) # Parsea el diccionario result a formato json
+            datos=web.input()
+            if datos['token']=="1234":
+                result=[]
+                result2={}
 
-    @staticmethod
-    def actionGet():
-        try:
-            result = {} # crear diccionario vacio
-            result['status'] = "200 Ok" # mensaje de status
-            file = 'static/csv/alumnos.csv' # define el archivo donde se almacenan los datos
-            with open(file,'r') as csvfile: # abre el archivo en modo lectura
-                reader = csv.DictReader(csvfile) # toma la 1er fila para los nombres
-                alumnos = [] # array para almacenar todos los alumnos
-                for row in reader: # recorre el archivo CSV fila por fila
-                    fila = {} # Genera un diccionario por cada registro en el csv
-                    fila['matricula'] = row['matricula'] # optine la matricula y la agrega al diccionario
-                    fila['nombre'] = row['nombre'] # optione el nombre y lo agrega al diccionario
-                    fila['primer_apellido'] = row['primer_apellido'] # optiene el primer_apellido
-                    fila['segundo_apellido'] = row['segundo_apellido'] # optiene el segundo apellido
-                    fila['carrera'] = row['carrera'] # optiene la carrera
-                    alumnos.append(fila) # agrega el diccionario generado al array alumnos
-                result['alumnos'] = alumnos # agrega el array alumnos al diccionario result
-            return result # Regresa el diccionario generado
-        except  Exception as e:
-            result = {}
-            result['status'] = "Error"
-            return result # Regresa el diccionario generado
+                # Mostrar todos los registros
+                if datos['action']=="get":
+                    with open('static/csv/alumnos.csv','r') as csvfile:
+                        reader = csv.DictReader(csvfile)
+                        for row in reader:
+                            result.append(row)
+                            result2['Version']="0.1.0"
+                            result2['status']="200 OK"
+                            result2['alumnos']=result
+                    return json.dumps(result2)
+                elif datos['action']=="search":
+                    consulta={}
+                    consulta['Version']="0.2.0"
+                    consulta['status']="200 ok"
+                    result="matricula,nombre,primer_apellido,segundo_apellido,carrera\n"
+                    with open('static/csv/alumnos.csv','r') as csvfile:
+                        reader = csv.DictReader(csvfile)
+                        result = []
+                        for row in reader:
+                            if str(row['matricula'])==datos['matricula']:
+                                result.append(row)
+                    return json.dumps(result)
+
+                #Insertar un registro nuevo
+                elif datos["action"] == "put":
+                    m1 = datos["matricula"]
+                    m2 = datos["nombre"]
+                    m3 = datos["primer_apellido"]
+                    m4 = datos["segundo_apellido"]
+                    m5 = datos["carrera"]
+                    result = []
+                    result.append(m1)
+                    result.append(m2)
+                    result.append(m3)
+                    result.append(m4)
+                    result.append(m5)
+                    with open ('static/csv/alumnos.csv','a+', newline = '') as csvfiles:
+                        writer = csv.writer(csvfiles)
+                        writer.writerow(result)
+                    return("Realizado")
+
+                #Borrar un registro
+                elif datos['action'] == "delete":
+                    with open ('static/csv/alumnos.csv','r') as csvfiles:
+                        reader =csv.DictReader(csvfiles)
+                        lo = []
+                        validator = 0
+                        for row in reader:
+                            result = []
+                            if  str(row['matricula']) == datos['matricula']:
+                                with open ('static/csv/alumnos.csv','w') as csvfile:
+                                    writer = csv.writer(csvfile)
+                                    writer.writerow(row)
+                                    print("ok")
+                            else:
+                                line1 = row['matricula']
+                                line2 = row['nombre']
+                                line3 = row['primer_apellido']
+                                line4 = row['segundo_apellido']
+                                line5 = row['carrera']
+                                result.append(line1)
+                                result.append(line2)
+                                result.append(line3)
+                                result.append(line4)
+                                result.append(line5)
+                                lo.append(result)
+                            with open ('static/csv/alumnos.csv','a+', newline = '') as csvfiles:
+                                writer = csv.writer(csvfiles)
+                                writer.writerow(result)
+                            if validator == 0:
+                                result.append("No existe el valor")
+                        return json.dumps("Realizado")
+
+                #Actualizar un registro
+                elif datos['action'] == "update":
+                    with open ('static/csv/alumnos.csv','r') as csvfiles:
+                        reader =csv.DictReader(csvfiles)
+                        lo = []
+                        validator = 0
+                        for row in reader:
+                            result = []
+                            if  str(row['matricula']) == datos['matricula']:
+                                with open ('static/csv/alumnos.csv','w') as csvfile:
+                                    writer = csv.writer(csvfile)
+                                    writer.writerow(row)
+                                    v1 = datos["matricula"]
+                                    v2 = datos["nombre"]
+                                    v3 = datos["primer_apellido"]
+                                    v4 = datos["segundo_apellido"]
+                                    v5 = datos["carrera"]
+                                    result.append(v1)
+                                    result.append(v2)
+                                    result.append(v3)
+                                    result.append(v4)
+                                    result.append(v5)
+                                    lo.append(result)
+                            else:
+                                fila1 = row['matricula'] 
+                                fila2 = row['nombre']
+                                fila3 = row['primer_apellido']
+                                fila4 = row['segundo_apellido']
+                                fila5 = row['carrera']
+                                result.append(fila1)
+                                result.append(fila2)
+                                result.append(fila3)
+                                result.append(fila4)
+                                result.append(fila5)
+                                lo.append(result)
+                        with open ('static/csv/alumnos.csv','a+', newline = '') as csvfiles:
+                            writer = csv.writer(csvfiles)
+                            for x in lo:
+                                writer.writerow(x)
+                        if validator == 0:
+                            result.append("No se encontro el dato")
+                    return json.dumps("Actualizado")
+
+                #Comando no encontrado
+                else:
+                    result2={}
+                    result2['status']="Command not found"
+                    return json.dumps(result2)
+
+            else:
+                result={}
+                result['status']="Los datos insertados son incorrectos"
+                return json.dumps(result)
+
+        except Exception as e:
+            result={}
+            text= "Oh oh... Esto no deberia pasar{}".format(e.args)
+            result  ['status'] = text 
+            return json.dumps(result)
